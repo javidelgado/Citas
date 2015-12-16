b321ea9e5f1091c60bae5cf46a21ebf7e0de64b7
jastella <jorge@fabricaweb.es>
2015-12-16 19:37:14 +0100

    Commit Inicial

---
+++ app/src/main/java/es/fabricaweb/www/citas/LoginActivity.java
@@ -0,0 +1,16 @@
+package es.fabricaweb.www.citas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrador on 16/12/15.
+ */
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
